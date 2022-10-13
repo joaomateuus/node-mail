5052a16b8c36cec23ce4ebf9c357e6a131802dcd
Joao Mateus <joao.amaral@ifam.edu.br>
2022-10-13 14:55:39 -0300

    fix: parsing to json and adding messsages endpoint

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,8 +2,8 @@ import path from "path";
 import express, {Express, NextFunction, Request, Response} from "express";
 import {serverInfo} from "./ServerInfo";
 import * as IMAP from "./IMAP";
-//import * as SMTP from "./SMTP";
-//import * as Contacts from "./Contacts";
+import * as SMTP from "./SMTP";
+import * as Contacts from "./Contacts";
 //import {IContact} from "./Contacts"
 
 //using express
@@ -25,6 +25,7 @@ app.get("/mailboxes", async(inRequest: Request, inResponse: Response) => {
     try {
         const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
         const mailboxes: IMAP.Imailbox[] = await imapWorker.listMailboxes();
+        inResponse.json(mailboxes);
     } catch (error) {
         console.log(error);
     }
@@ -54,6 +55,27 @@ app.delete("/messages/:mailbox/:id", async(inRequest: Request, inResponse: Respo
         console.log(error);
     }
 })
+
+app.post("/messages", async(inResquest: Request, inResponse:Response) => {
+   try {
+        const smtpWorker: IMAP.Worker = new SMTP.Worker(serverInfo);
+        await smtpWorker.sendMessage(inRequest.body);
+        inResponse.send("Message was sucessfully send");
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.get("/contacts", async(inRequest: Request, inResponse: Response) => {
+    try {
+        const workerContacts: Contacts.Worker = new Contacts.Worker();
+        const contacts: IContact[] = await workerContacts.listContacts();
+        inResponse.json(contacts);
+    } catch (error) {
+        console.log(error);
+    }
+})
+
 
 
 
