694ad7fb1386362c887378fc494088a20ac6619d
Joao Mateus <joao.amaral@ifam.edu.br>
2022-10-13 19:48:33 -0300

    feat: serverInfo file

--- src/server/ServerInfo.ts
+++ src/server/ServerInfo.ts
@@ -1,3 +1,20 @@
-export default {
-    
-}
+const path = require("path");
+const fs = require("fs");
+
+export interface IserverInfo {
+    smtp: {
+        host: string, port: number,
+        auth: {user: string, pass: string}
+    }
+    imap: {
+        host: string, port: number,
+        auth: {user: string, pass: string}
+    }
+};
+
+export let serverInfo: IserverInfo;
+
+const rawInfo: string = fs.readFileSync(path.join(__dirname, "../serverInfo.json"));
+serverInfo = JSON.parse(rawInfo);
+
+
